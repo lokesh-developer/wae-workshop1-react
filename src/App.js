97c3c89f98a2fce6431903af4ed3423c45a94d1c
Lokesh Kabra <kabral1999@gmail.com>
2024-03-24 16:53:00 +0530

    :)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Table } from './components/Table';
+import axios from 'axios';
 
-function App() {
-  const addTask = () => {
+function App()
+{
+  const [input, setInput] = useState("")
+  const [taskName, setTaskName] = useState([])
+
+  console.log(input)
+  useEffect(() =>
+  {
+    console.log("lokesh")
+  }, [input])
+
+  const addTask = async () =>
+  {
+    try {
+      const addData = await axios.post("https://4000-lokeshdevel-waeworkshop-wmkj7h8ck45.ws-us110.gitpod.io/addTask", { name: input })
+      console.log(addData, input)
+      setTaskName([...taskName, input])
+      setInput("")
+    } catch (error) {
+      console.log(error)
+    }
 
   }
+  useEffect(() => {
+    const loadData = async () => {
+      let taskss = []
+      try {
+        const tasks = await axios.get("https://4000-lokeshdevel-waeworkshop-wmkj7h8ck45.ws-us110.gitpod.io/get")
+        tasks.data.map((t) => {
+          taskss.push({name: t.name, id: t._id, completed: t.completed})
+        })
+          setTaskName(taskss)
+        
+  
+  
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    loadData()
+  }, [])
+  
   return (
     <>
       <div className="box">
         <h2>Task App</h2>
       </div>
-      <div class="box task-box">
-        <input name="task-name" id="task-name" onChange={addTask()} placeholder="New Task" />
-        <button id="add-task">Add Task</button>
+      <div className="box task-box">
+        <input name="task-name" id="task-name" value={input} onChange={(e) => setInput(e.target.value)} placeholder="New Task" />
+        <button id="add-task" onClick={addTask}>Add Task</button>
       </div>
-      <Table />
-      <Table />
-      <Table />
+      <Table taskName={taskName} />
     </>
   );
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,13 +1,40 @@
-export const Table = () => {
-    return (
-        <div class="box table-box">
-        <table>
+import axios from "axios"
+import { useRef } from "react"
+
+export const Table = ({ taskName }) =>
+{
+  const taskNameRef = useRef()
+  const completeTask = async (e) =>
+  {
+    try {
+      const completeIt = await axios.post("https://4000-lokeshdevel-waeworkshop-wmkj7h8ck45.ws-us110.gitpod.io/completeTask", { id: e.target.id })
+      e.target.parentElement.parentElement.querySelector('#taskHu').classList.add("completed")
+      console.log(e)
+      e.target.disabled = true
+    } catch (error) {
+      console.log(error)
+    }
+   
+  }
+
+  return (
+    <div className="box table-box">
+      <table>
+        <tbody>
           <tr>
             <th>Task Name</th>
             <th>Done</th>
             <th>Delete</th>
           </tr>
-        </table>
-      </div>
-    )
+          {taskName?.map((t) => (
+            <tr>
+              <td id="taskHu" className={t?.completed && "completed"} ref={taskNameRef}>{t.name}</td>
+              <td><button disabled={t?.completed && true} id={t.id}  onClick={(e) => completeTask(e)} >Complete</button></td>
+              <td><button id="delete-task" >Delete</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
 }
